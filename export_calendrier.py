e3e37bf36c41215bc2c3c35b6b269be15bb77f74
johen <johennequin@yahoo.fr>
2024-03-23 21:45:07 +0100

    openpyxl

--- export_calendrier.py
+++ export_calendrier.py
@@ -7,18 +7,35 @@
 @Desc    :   recuperation du calendrier sur cyu.fr
 """
 import locale
+import os
 import datetime
-import keepasshttp
+import sys
+import tempfile
 import logging
 import time
+import pickle
+import subprocess
+
+import keepassxc_proxy_client
+import keepassxc_proxy_client.protocol
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from tabulate import tabulate
-from reportlab.platypus import SimpleDocTemplate, Paragraph
-from reportlab.lib.styles import getSampleStyleSheet
-from reportlab.lib.pagesizes import A4
-import tempfile
-import win32api
+from selenium.webdriver.firefox.options import Options
+# from tabulate import tabulate
+# from reportlab.platypus import SimpleDocTemplate, Paragraph
+# from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+# from reportlab.lib.pagesizes import A4
+from openpyxl import Workbook
+from openpyxl.worksheet.properties import PageSetupProperties
+from openpyxl.worksheet.worksheet import Worksheet
+
+
+class Dict2Class(object):
+
+    def __init__(self, my_dict):
+        for key in my_dict:
+            setattr(self, key, my_dict[key])
 
 
 def get_credential(url):
@@ -28,73 +45,107 @@ def get_credential(url):
     :return:
     """
     # get single credential
-    credential = keepasshttp.get(url)
+    connection = keepassxc_proxy_client.protocol.Connection()
+    connection.connect()
+    if not os.path.exists(os.path.join(os.getenv('HOME'), '.ssh', 'python_keepassxc')):
+        connection.associate()
+        name, public_key = connection.dump_associate()
+        print("Got connection named '", name, "' with key", public_key)
+        with open(os.path.join(os.getenv('HOME'), '.ssh', 'python_keepassxc'), 'wb') as fic:
+            fic.write(public_key)
+    with open(os.path.join(os.getenv('HOME'), '.ssh', 'python_keepassxc'), 'rb') as fic:
+        public_key = fic.read()
+    connection.load_associate('python', public_key)
+    connection.test_associate()
+    credentials = connection.get_logins(url)
+    credential = Dict2Class(credentials[0])
     logging.debug(credential.login)
     return credential
 
 
 if __name__ == '__main__':
     logging.basicConfig(encoding='utf-8', level=logging.INFO)
-    locale.setlocale(locale.LC_ALL, 'french_france')
-    driver = webdriver.Firefox()
+    my_creds = get_credential("https://cyu.fr/")
+    locale.setlocale(locale.LC_ALL, 'fr_FR.utf8')
+    options = Options()
+    options.binary_location = '/usr/lib/firefox/firefox'
 
-    jours = list(datetime.date(2001, 1, i).strftime('%A') for i in range(1, 6))
+    jours = list(datetime.date(2001, 1, i).strftime('%A') for i in range(1, 7))
     start_date = datetime.date.today()
     if start_date.weekday() != 0:
         start_date += datetime.timedelta(days=7 - start_date.weekday())
-    e_d_t = [jours]
+    dates = jours
     for ind_i, _ in enumerate(jours):
         n_date = start_date + datetime.timedelta(days=ind_i)
-        e_d_t[0][ind_i] += n_date.strftime(' %d/%m')
-    for ind_i in range(5):
-        e_d_t.append([])
-        for ind_j in range(len(jours)):
-            e_d_t[ind_i+1].append('')
+        dates[ind_i] += n_date.strftime(' %d/%m')
+    # for ind_i in range(5):
+    #     dates.append([])
+    #     for ind_j in range(len(jours)):
+    #         dates[ind_i+1].append('')
+    logging.debug(dates)
+    if os.path.exists('e_d_t.dat'):
+        # vérification de la date d'export
+        now = datetime.datetime.now()
+        debut = now - datetime.timedelta(days=6)
+        st = os.stat('e_d_t.dat')
+        date_f = datetime.datetime.fromtimestamp(st.st_mtime)
+        if date_f < debut:
+            os.remove('e_d_t.dat')
+    if os.path.exists('e_d_t.dat'):
+        # export encore valide
+        with open('e_d_t.dat', 'rb') as driver_dat:
+            e_d_t = pickle.load(driver_dat)
+    else:
+        # pas d'export disponible → on récupère depuis cyu.fr
+        driver = webdriver.Firefox()
+        my_creds = get_credential("https://cyu.fr/")
+        my_creds2 = get_credential("https://services-web.cyu.fr/calendar/cal")
 
-    my_creds = get_credential("https://cyu.fr/")
-    my_creds2 = get_credential("https://services-web.cyu.fr/calendar/cal")
+        URL = "https://services-web.cyu.fr/calendar/"
+        LOGIN_ROUTE = "LdapLogin/"
+        DATA_ROUTE = "cal?vt=agendaWeek&dt=%s&et=student&fid0=%s" % (start_date.isoformat(), my_creds2.login)
 
-    URL = "https://services-web.cyu.fr/calendar/"
-    LOGIN_ROUTE = "LdapLogin/"
-    DATA_ROUTE = "cal?vt=agendaWeek&dt=%s&et=student&fid0=%s" % (start_date.isoformat(), my_creds2.login)
+        driver.get(URL+LOGIN_ROUTE)
 
-    driver.get(URL+LOGIN_ROUTE)
+        uname = driver.find_element(By.ID, "Name")
+        uname.send_keys(my_creds.login)
+        upswd = driver.find_element(By.ID, "Password")
+        upswd.send_keys(my_creds.password)
+        driver.find_element(By.CSS_SELECTOR, ".loginBtn").click()
+        driver.get(URL+DATA_ROUTE)
+        time.sleep(1)
 
-    uname = driver.find_element(By.ID, "Name")
-    uname.send_keys(my_creds.login)
-    upswd = driver.find_element(By.ID, "Password")
-    upswd.send_keys(my_creds.password)
-    driver.find_element(By.CSS_SELECTOR, ".loginBtn").click()
-    driver.get(URL+DATA_ROUTE)
-    time.sleep(1)
+        e_d_t = [['Horaire', 'Cours / TD', 'Salle', 'Prof.']]
+        for ind_i, col in enumerate(driver.find_elements(By.CSS_SELECTOR, ".fc-content-col")):
+            e_d_t.append([dates[ind_i], '', '', ''])
+            for ind_j, output in enumerate(col.find_elements(By.XPATH, value="(.//*[contains(@class, 'fc-content')])")):
+                if output.text:
+                    heure_i, sans_lignes = output.text.split('\n', 1)
+                    contenu_i, suite_i = sans_lignes.split('\nCH', 1)
+                    salle_i = 'CH' + suite_i.split('p\n')[0] + 'p'
+                    salle_i = '\n'.join(salle_i.split(' ', 1))
+                    salle_i = '\n'.join(salle_i.rsplit(' ', 1))
+                    try:
+                        prof_i = suite_i.split('p\n')[1]
+                    except IndexError:
+                        prof_i = ''
+                    e_d_t.append(['- %s' % heure_i, contenu_i, salle_i, prof_i])
+        driver.close()
+        with open('e_d_t.dat', 'wb') as driver_dat:
+            pickle.dump(e_d_t, driver_dat)
 
-    for ind_i, col in enumerate(driver.find_elements(By.CSS_SELECTOR, ".fc-content-col")):
-        for ind_j, output in enumerate(col.find_elements(By.XPATH, value="(.//*[contains(@class, 'fc-content')])")):
-            if output.text:
-                e_d_t[ind_j+1][ind_i] = output.text
-    text_edt = tabulate(e_d_t, headers='firstrow')
-    logging.debug('\'%s' % text_edt)
-
-    driver.close()
-
-    pdf_file_name = tempfile.mktemp(".pdf")
-
-    styles = getSampleStyleSheet()
-    h1 = styles["h1"]
-    normal = styles["Code"]
-    normal.fontSize = 6
-
-    doc = SimpleDocTemplate(
-        pdf_file_name,
-        pagesize=(A4[1],A4[0]),
-        leftMargin=0, rightMargin=0)
-
-    text = text_edt.splitlines()
-    story = []
-    for line in text:
-        # reportlab expects to see XML-compliant
-        #  data; need to escape ampersands space and so on.
-        story.append(Paragraph(line.replace(' ', '&nbsp;'), normal))
-
-    doc.build(story)
-    win32api.ShellExecute(0, "open", pdf_file_name, None, ".", 0)
+    wb = Workbook()
+    ws = wb.active
+    for ligne in e_d_t:
+        ws.append(ligne)
+    ws.sheet_properties.pageSetUpPr = PageSetupProperties(fitToPage=True, autoPageBreaks=False)
+    dims = {}
+    for row in ws.rows:
+        for cell in row:
+            if cell.value:
+                dims[cell.column] = max([len(ligne) for ligne in str(cell.value).splitlines()]) + 5
+    for col, value in dims.items():
+        ws.column_dimensions[chr(64 + col)].width = value
+    Worksheet.set_printer_settings(ws, paper_size=9, orientation='landscape')
+    wb.save("e_d_t.xlsx")
+    subprocess.run(['open', "e_d_t.xlsx"], check=False)
