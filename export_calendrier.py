0b37bf31397eba1c42f215be216c9cbbace789c1
johen <johenfr@free.fr>
2024-09-22 15:28:01 +0200

    ajout autre etudiant

--- export_calendrier.py
+++ export_calendrier.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """
-@File    :   export_calendrier_gab.py
+@File    :   export_calendrier.py
 @Time    :   2023/09/23 11:38:01
 @Version :   1.0
 @Desc    :   recuperation du calendrier sur cyu.fr
@@ -27,6 +27,10 @@ from openpyxl.worksheet.worksheet import Worksheet
 from openpyxl.styles import PatternFill
 from openpyxl.styles import Font
 
+import tkinter.messagebox as msg
+
+etudiant="Gabriel"
+
 
 class Dict2ClassEmpty(object):
     def login(self):
@@ -48,6 +52,7 @@ def get_credential(url):
     :param url:
     :return:
     """
+    global etudiant
     # get single credential
     connection = keepassxc_proxy_client.protocol.Connection()
     connection.connect()
@@ -64,19 +69,26 @@ def get_credential(url):
     connection.test_associate()
     credentials = connection.get_logins(url)
     if credentials:
-        credential = Dict2Class(credentials[0])
-        logging.debug(credential.login)
-        return credential
-    else:
-        return Dict2ClassEmpty()
+        for gugusse in credentials:
+            wanted_etudiant = "e-%s" % etudiant[0].lower()
+            if wanted_etudiant in gugusse["login"]:
+                credential = Dict2Class(gugusse)
+                logging.debug(credential.login)
+                return credential
+    return Dict2ClassEmpty()
 
 
 if __name__ == "__main__":
+    for choix in ["Gabriel", "Louis-Joseph"]:
+        if msg.askyesno("Etudiant?", choix):
+            etudiant = choix
+            break
     logging.basicConfig(encoding="utf-8", level=logging.INFO)
-    my_creds = get_credential("https://cyu.fr/")
+    my_creds = Dict2ClassEmpty()
     locale.setlocale(locale.LC_ALL, "fr_FR.utf8")
     options = Options()
     options.binary_location = "/usr/lib/firefox/firefox"
+    driver = None
 
     jours = list(datetime.date(2001, 1, i).strftime("%A") for i in range(1, 7))
     start_date = datetime.date.today()
@@ -87,27 +99,27 @@ if __name__ == "__main__":
         n_date = start_date + datetime.timedelta(days=ind_i)
         dates[ind_i] += n_date.strftime(" %d/%m")
     logging.debug(dates)
-    if os.path.exists("e_d_t.dat"):
+    if os.path.exists("e_d_t_%s.dat" % etudiant.lower()):
         # vérification de la date d'export
         now = datetime.datetime.now()
         debut = now - datetime.timedelta(days=6)
-        st = os.stat("e_d_t.dat")
+        st = os.stat("e_d_t_%s.dat" % etudiant.lower())
         date_f = datetime.datetime.fromtimestamp(st.st_mtime)
         if date_f < debut:
-            os.remove("e_d_t.dat")
-    if os.path.exists("e_d_t.dat"):
+            os.remove("e_d_t_%s.dat" % etudiant.lower())
+    if os.path.exists("e_d_t_%s.dat" % etudiant.lower()):
         # export encore valide
-        with open("e_d_t.dat", "rb") as driver_dat:
+        with open("e_d_t_%s.dat" % etudiant.lower(), "rb") as driver_dat:
             e_d_t = pickle.load(driver_dat)
     else:
         # pas d'export disponible → on récupère depuis cyu.fr
-        driver = webdriver.Firefox()
         my_creds = get_credential("https://cyu.fr/")
         my_creds2 = get_credential("https://services-web.cyu.fr/calendar/cal")
-
+        
+        driver = webdriver.Firefox()
         URL = "https://services-web.cyu.fr/calendar/"
         LOGIN_ROUTE = "LdapLogin/"
-        DATA_ROUTE = "cal?vt=agendaWeek&dt=%s&et=student&fid0=%s" % (start_date.isoformat(), my_creds2.login)
+        DATA_ROUTE = "cal?vt=agendaWeek&dt=%s&et=student&fid0=%s" % (start_date.isoformat(), my_creds2.password)
 
         driver.get(URL + LOGIN_ROUTE)
 
@@ -125,45 +137,76 @@ if __name__ == "__main__":
             for ind_j, output in enumerate(col.find_elements(By.XPATH, value="(.//*[contains(@class, 'fc-content')])")):
                 if output.text:
                     heure_i, sans_lignes = output.text.split("\n", 1)
+                    prof_i = ""
                     try:
                         contenu_i, suite_i = sans_lignes.split("\nCH", 1)
+                        salle_i = "CH" + suite_i.split("p\n")[0] + "p"
+                        salle_i = "\n".join(salle_i.split(" ", 1))
+                        salle_i = "\n".join(salle_i.rsplit(" ", 1))
                     except ValueError:
-                        print(sans_lignes)
-                        contenu_i, suite_i = sans_lignes.split("\n", 1)
-                    salle_i = "CH" + suite_i.split("p\n")[0] + "p"
-                    salle_i = "\n".join(salle_i.split(" ", 1))
-                    salle_i = "\n".join(salle_i.rsplit(" ", 1))
+                        try:
+                            sans_lignes_1 = sans_lignes.split("\n", 1)[0]
+                            sans_lignes_2 = sans_lignes.split(")\n", 1)[1]
+                            sans_lignes = "\n".join([sans_lignes_1, sans_lignes_2])
+                        except IndexError:
+                            pass
+                        contenu_i = ""
+                        salle_i = ""
+                        suite_i = ""
+                        for site in ["NEU", "TUR"]:
+                            try:
+                                contenu_i, suite_i = sans_lignes.split("\n%s" % site, 1)
+                                salle_i = site + suite_i.split("p\n")[0] + "p"
+                                break
+                            except ValueError:
+                                continue
+                        if not contenu_i:
+                            if "à distance" in sans_lignes:
+                                reconstruction = sans_lignes.split("\n")
+                                reconstruction.pop()
+                                prof_i = reconstruction.pop()
+                                contenu_i = "\n".join(reconstruction)
+                                salle_i = "à la maison"
+                            else:
+                                contenu_i, suite_i = sans_lignes.split("\n", 1)
+                                salle_i = suite_i.split("p\n")[0] + "p"
                     try:
-                        prof_i = suite_i.split("p\n")[1]
+                        if salle_i != "à la maison":
+                            prof_i = suite_i.split("p\n")[1].split('\n')[0]
                     except IndexError:
-                        prof_i = ""
+                        pass
                     e_d_t.append(["- %s" % heure_i, contenu_i, salle_i, prof_i, ""])
-        driver.close()
-        with open("e_d_t.dat", "wb") as driver_dat:
+        with open("e_d_t_%s.dat" % etudiant.lower(), "wb") as driver_dat:
             pickle.dump(e_d_t, driver_dat)
 
     wb = Workbook()
     ws = wb.active
     blueFill = PatternFill(start_color="FFAAAAFF", end_color="FFAAAAFF", fill_type="solid")
-    if ws is not None:
-        for ligne in e_d_t:
-            ws.append(ligne)
-        for row in ws.rows:
-            for cell in row:
-                cell.font = Font(bold=True)
-            break
+    if ws is None:
+        exit(1)
+    for ligne in e_d_t:
+        ws.append(ligne)
+    for row in ws.rows:
+        for cell in row:
+            cell.font = Font(bold=True)
+        break
 
-        ws.sheet_properties.pageSetUpPr = PageSetupProperties(fitToPage=True, autoPageBreaks=False)
-        dims = {}
-        for row in ws.rows:
-            for cell in row:
-                if cell.value:
-                    dims[cell.column] = max([len(ligne) for ligne in str(cell.value).splitlines()]) + 5
-                if not row[2].value:
-                    cell.fill = blueFill
-                    cell.font = Font(bold=True)
-        for col, value in dims.items():
-            ws.column_dimensions[chr(64 + col)].width = value
+    ws.sheet_properties.pageSetUpPr = PageSetupProperties(fitToPage=True, autoPageBreaks=False)
+    dims = {}
+    for row in ws.rows:
+        for cell in row:
+            if cell.value:
+                dims[cell.column] = max([len(ligne) for ligne in str(cell.value).splitlines()]) + 5
+            if not row[2].value:
+                cell.fill = blueFill
+                cell.font = Font(bold=True)
+    for col, value in dims.items():
+        ws.column_dimensions[chr(64 + col)].width = value
+    if etudiant == "Gabriel":
         Worksheet.set_printer_settings(ws, paper_size=9, orientation="landscape")
-        wb.save("e_d_t.xlsx")
-        subprocess.run(["open", "e_d_t.xlsx"], check=False)
+    else:
+        Worksheet.set_printer_settings(ws, paper_size=9, orientation="portrait")
+    wb.save("e_d_t_%s.xlsx" % etudiant.lower())
+    subprocess.run(["open", "e_d_t_%s.xlsx" % etudiant.lower()], check=False)
+    if driver:
+        driver.close()
